4c4d360f35e98dee45d3033c92ea41e4b020f161
KatiaVasileva <eka.vasileva@gmail.com>
2024-11-21 19:04:59 +0500

    feature/app

---
+++ skypro-fitness/src/pages/CoursePage.tsx
@@ -0,0 +1,15 @@
+import { Outlet, useParams } from "react-router-dom";
+import Course from "../components/Course/Course";
+import Header from "../components/Header/Header";
+
+export default function CoursePage() {
+  const { id } = useParams();
+
+  return (
+    <>
+      <Header />
+      <Course courseId={id} />
+      <Outlet />
+    </>
+  );
+}
